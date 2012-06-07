4d33881b4e69c33e2370dbd2883d6fbef17bdf55
Thorsten Fischer <thorsten@froschi.org>
2012-06-07 15:49:38 +0200

    Include libjpeg-turbo recipe here, not libjpeg-turbo8.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,13 @@
-packages = %w/
-  libjpeg62
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid"
+  packages |= %w/libjpeg62/
+when "precise"
+  include_recipe "libjpeg-turbo"
+
+  packages |= %w/libjpeg8/
+end
 
 packages.each do |pkg|
   package pkg do
