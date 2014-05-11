217128b12b0a2fc07e892904f9aeeb469fa7f209
Joe Bergantine <jbergantine@gmail.com>
2014-05-11 09:58:54 -0600

    Added readme, metadata, changelog

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "thorsten@froschi.org"
 license          "Apache 2.0"
 description      "Installs/Configures libjpeg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 
 %w/ubuntu/.each { |os| supports os }
