6bbd705cf504d1bbb556e9fea64bb2ec6f42915f
Joe Bergantine <jbergantine@gmail.com>
2014-05-11 14:43:25 -0600

    more specific about ubuntu version support

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures libjpeg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-%w/ubuntu/.each { |os| supports os }
+supports 'ubuntu', '>= 10.04'
