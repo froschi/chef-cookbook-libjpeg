83bb3caf4bdb36d1b637eb340f5cd02b398d8643
Joe Bergantine <jbergantine@gmail.com>
2014-05-11 19:57:10 -0600

    version bump to 1.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "thorsten@froschi.org"
 license          "Apache 2.0"
 description      "Installs/Configures libjpeg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
 supports 'ubuntu', '>= 10.04'
