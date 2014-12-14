a04329e74cffc9aafdd6ba9bc3e0a050dd47380c
Joe Bergantine <jbergantine@gmail.com>
2014-12-14 10:24:09 -0700

    updated changelog, metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'libjpeg'
-maintainer       "Thorsten Fischer"
-maintainer_email "thorsten@froschi.org"
+maintainer       "Joe Bergantine"
+maintainer_email "joe@joebergantine.com"
 license          "Apache 2.0"
 description      "Installs/Configures libjpeg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
